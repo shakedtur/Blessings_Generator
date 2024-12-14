09b586d64bdc44125cfb68dc77b694384c0190e6
tammyh9939 <137795282+tammyh9939@users.noreply.github.com>
2024-12-14 20:14:43 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 # Example external function
-def process_data(name, age, hobby, email, gender):
-    return f"Processed:test Name - {name}, Age - {age}, Hobby - {hobby}, Email - {email}, Gender - {gender}"
+def process_data(name, age, hobby, kinship, style, otherStyle,  email, gender, emoji, nameWish):
+    return f"Processed:test Name - {name}, Age - {age}, Hobby - {hobby}, kinship - {kinship}, style - {style}, otherStyle - {otherStyle}, Email - {email}, Gender - {gender}, emoji - {emoji}, nameWish - {nameWish}"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -15,10 +15,19 @@ def index():
         age = request.form.get('age')
         hobby = request.form.get('hobby')
         email = request.form.get('email')
+        kinship = request.form.get('kinship')
+        style = request.form.get('style')
+        otherStyle = request.form.get('otherStyle')
         gender = request.form.get('gender')
+        emoji = request.form.get('emoji')
+        nameWish = request.form.get('nameWish')
+        if style == 'other':
+            result = otherStyle
+        else:
+            result = style
 
         # Process the data
-        result = process_data(name, age, hobby, email, gender)
+        result = process_data(name, age, hobby, email, kinship, style, otherStyle, gender, emoji, nameWish)
 
     return render_template('web.html', result=result)
 
