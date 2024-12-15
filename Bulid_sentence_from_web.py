576120f8d6385eb468216d26e6596ef158960ea2
tammyh9939 <137795282+tammyh9939@users.noreply.github.com>
2024-12-15 20:46:35 +0200

    Add files via upload

---
+++ Bulid_sentence_from_web.py
@@ -0,0 +1,72 @@
+
+def check_input_in_range(min_value, max_value,num):
+    """
+      Validates user input to be within a given numeric range.
+      :param min_value: The minimum acceptable value.
+      :param max_value: The maximum acceptable value.
+      :return: The valid input within the range.
+      """
+
+    while True:
+        try:
+            value =num
+            if min_value <= value <= max_value:
+                return value
+            else:
+                return (f"Input is out of range. Please enter a number between {min_value} and {max_value}.")
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+
+def check_text_input(user_text):
+    """
+    Validates that the user input contains only alphabetic characters.
+    :return: The valid text input.
+    """
+    while True:
+        text = user_text
+        if text.isalpha():
+           #print(f"Valid input: {text}")
+            return text
+        else:
+            return ("Invalid input. Please enter letters only.")
+
+#1
+def define_reqest_text_gemini():
+    text= "write a Birthday greeting consider the following parameters:\n"
+    return str(text)
+
+#2
+def Recipient_name(text,name):
+    text += f"\nRecipient's Name: {name} "
+    return str(text)
+
+#3
+def recipient_age(text,age):
+    text += f"\nRecipient's Age: {age} "
+    return text
+#4
+def define_recipient_hobbies(text, hobbies):
+    text += f"\nRecipient's Hobbies: {hobbies}"
+    return text
+
+#7
+def define_sex(text,sex):
+    text += f"\nThe recipient gender is: {sex}"
+    return text
+
+#8
+def define_style(text, style):
+    text += f"\nThe greeting style: {style}"
+    return text
+
+#9
+def define_emoji(text,emoji):
+    text += f"\nAdd an emoji to the greeting: {emoji}"
+    return text
+
+#10
+def define_name_wish(text, nameWish):
+    text += f"\nThere I wish the blessing: {nameWish}"
+    return text
+
---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+# Example external function
+def process_data(name, age, hobby, gender ,style , emoji, nameWish):
+    import Bulid_sentence_from_web as web_page
+    web_sentence= web_page.define_reqest_text_gemini()
+    web_sentence= web_page.Recipient_name(web_sentence,name)
+    web_sentence= web_page.recipient_age(web_sentence,age)
+    web_sentence= web_page.define_recipient_hobbies(web_sentence,hobby)
+    web_sentence= web_page.define_style(web_sentence, style)
+    web_sentence= web_page.define_sex(web_sentence,gender)
+    web_sentence= web_page.define_emoji(web_sentence,emoji)
+    web_sentence= web_page.define_name_wish(web_sentence,nameWish)
+
+    print(f"##############\n{web_sentence}\n###############\n")
+    # import Gemini_messege
+    # answer= Gemini_messege.belssing_request(web_sentence)
+    # print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+    # print(answer)
+    # print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+    # TODO: add saving in data base
+    #request_DB =jsonify({"result": result})
+    # TODO: send message to gemini via API
+    #return answer + f"\nProcessed:test Name - {name}, Age - {age}, Hobby - {hobby}, Email - {email}, Gender - {gender}"
+
+    return web_sentence
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = None
+    if request.method == 'POST':
+        # Get data from the form
+        name = request.form.get('name')
+        age = request.form.get('age')
+        hobby = request.form.get('hobby')
+        style = request.form.get('style')
+        otherStyle = request.form.get('otherStyle')
+        gender = request.form.get('gender')
+        emoji = request.form.get('emoji')
+        nameWish = request.form.get('nameWish')
+        if style == 'other':
+           style = otherStyle
+
+        # Process the data
+        result = process_data(name, age, hobby, gender, style, emoji, nameWish)
+
+    return render_template('web.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
