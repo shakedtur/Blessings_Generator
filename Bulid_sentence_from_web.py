0b69484ff60f9418e12696923f68eae31cbdd460
shakedtur <59848826+shakedtur@users.noreply.github.com>
2024-12-16 22:51:59 +0200

    fix bugs
    improve message to gemini

---
+++ Bulid_sentence_from_web.py
@@ -0,0 +1,85 @@
+
+def check_input_in_range(min_value, max_value,num):
+    """
+      Validates user input to be within a given numeric range.
+      :param min_value: The minimum acceptable value.
+      :param max_value: The maximum acceptable value.
+      :return: The valid input within the range.
+      """
+
+    while True:
+        try:
+            value =num
+            if min_value <= value <= max_value:
+                return value
+            else:
+                return (f"Input is out of range. Please enter a number between {min_value} and {max_value}.")
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+
+def check_text_input(user_text):
+    """
+    Validates that the user input contains only alphabetic characters.
+    :return: The valid text input.
+    """
+    while True:
+        text = user_text
+        if text.isalpha():
+           #print(f"Valid input: {text}")
+            return text
+        else:
+            return ("Invalid input. Please enter letters only.")
+
+#1
+def define_reqest_text_gemini():
+    text= "write a Birthday greeting consider the following parameters:\n"
+    return str(text)
+
+#2
+def Recipient_name(text,name):
+    text += f"\nRecipient's Name: {name} "
+    return str(text)
+
+#3
+def recipient_age(text,age):
+    text += f"\nThe recipient celebrating {age} years old."
+    return text
+#4
+def define_recipient_hobbies(text, hobbies):
+    text += f"\nRecipient's Hobbies: {hobbies}"
+    return text
+#5
+def define_kinship(text,relationship):
+    #relationship = input("What is your relationship to the recipient? (e.g., friend, family member, coworker) ")
+    text += f"\nRelationship between the recipient to the author : {relationship}"
+    return text
+
+#6
+def define_style(text, style):
+    text += f"\nThe blessing style: {style}"
+    return text
+#7
+def define_sex(text,sex):
+    text += f"\nThe recipient gender is: {sex}"
+    return text
+
+#8
+def define_style(text, style):
+    text += f"\nThe greeting style: {style}"
+    return text
+
+#9
+def define_emoji(text,emoji):
+    text += f"\nAdd an nice emojis to the blessing: {emoji}"
+    return text
+
+#10
+def define_name_wish(text, nameWish):
+    text += f"\nThe name of the wishing blessing person sign at the end of the blessing is {nameWish}"
+    return text
+
+#11
+def define_language_belssing(text, lang_choise):
+    text += f"\nthe blessing gone be write at {lang_choise} language."
+    return str(text)
--- app.py
+++ app.py
@@ -1,20 +1,76 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
+
+def save_data_to_DB(data,answer,nameWish):
+    # TODO: add saving in data base
+
+    request_DB = str(jsonify(data).json)
+    from Mongo import Blesing_DB_mongo as mongo_DB
+    mongo_DB.save_text_in_mogoDB(answer,nameWish, request_DB)
+
+def send_message_to_gemini(message):
+    import Gemini_messege
+    gemini_response = Gemini_messege.belssing_request(message)
+    return gemini_response
+
+
 # Example external function
-def process_data(name, age, hobby, kinship, style, otherStyle,  email, gender, emoji, nameWish):
-    return f"Processed:test Name - {name}, Age - {age}, Hobby - {hobby}, kinship - {kinship}, style - {style}, otherStyle - {otherStyle}, Email - {email}, Gender - {gender}, emoji - {emoji}, nameWish - {nameWish}"
+def process_data(language, name, age, hobby, kinship, gender ,style , emoji, nameWish):
+    import Bulid_sentence_from_web as web_page
+
+    result = {
+        "language": language,
+        "name": name,
+        "age": age,
+        "hobby": hobby,
+        "kinship": kinship,
+        "gender": gender,
+        "style": style,
+        "emoji": emoji,
+        "nameWish": nameWish
+    }
+
+    web_sentence= web_page.define_reqest_text_gemini()
+    web_sentence= web_page.define_language_belssing(web_sentence, language)
+    web_sentence= web_page.Recipient_name(web_sentence,name)
+    web_sentence= web_page.recipient_age(web_sentence,age)
+    web_sentence= web_page.define_recipient_hobbies(web_sentence,hobby)
+    web_sentence= web_page.define_kinship(web_sentence, kinship)
+    web_sentence= web_page.define_style(web_sentence, style)
+    web_sentence= web_page.define_sex(web_sentence,gender)
+    web_sentence= web_page.define_emoji(web_sentence,emoji)
+    web_sentence= web_page.define_name_wish(web_sentence,nameWish)
+
+    print(f"##############\n{web_sentence}\n###############\n")
+
+
+    # TODO: send message to gemini via API
+    answer= send_message_to_gemini(web_sentence)
+
+    # TODO: add saving in data base
+    save_data_to_DB(result,answer,nameWish)
+
+    print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+    print(answer)
+    print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+
+
+    return answer
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     result = None
+    #error = None
     if request.method == 'POST':
         # Get data from the form
+        language = request.form.get('language') 
         name = request.form.get('name')
         age = request.form.get('age')
         hobby = request.form.get('hobby')
-        email = request.form.get('email')
         kinship = request.form.get('kinship')
         style = request.form.get('style')
         otherStyle = request.form.get('otherStyle')
@@ -22,12 +78,19 @@ def index():
         emoji = request.form.get('emoji')
         nameWish = request.form.get('nameWish')
         if style == 'other':
-            result = otherStyle
-        else:
-            result = style
+           style = otherStyle
+
+        #try:
+            # Convert age to integer if necessary for calculations
+        #    age = int(age)
+        #except ValueError:
+        #    error = "Age must be a number."
+
+        #if not error:  # Process data only if no errors
+        #    result = process_data(name, age, hobby, gender, style, emoji, nameWish)
 
         # Process the data
-        result = process_data(name, age, hobby, email, kinship, style, otherStyle, gender, emoji, nameWish)
+        result = process_data(language, name, age, hobby, kinship, gender, style, emoji, nameWish)
 
     return render_template('web.html', result=result)
 
