64a783388819963113113892e6daef912c93baee
Jawad Makhoul <110395806+JawadMakhoul@users.noreply.github.com>
2024-12-31 00:51:04 +0200

    Update SecretManager.py

--- SecretManager.py
+++ SecretManager.py
@@ -34,3 +34,28 @@ def get_secret():
     api_key = data["GeminiAPIKey"]
 
     return api_key
+
+def get_database_cred():
+
+    secret_name = "MongoDB"
+    region_name = "us-west-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    
+    return secret
